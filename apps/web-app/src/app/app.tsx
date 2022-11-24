e4db85054f5074adf8d6b9410291e8b961161bca
Santhosh Umapathi <Santhosh.Professional1@gmail.com>
2022-11-24 07:30:22 +0100

    FIX: Updated

--- apps/web-app/src/app/app.tsx
+++ apps/web-app/src/app/app.tsx
@@ -1,6 +1,9 @@
 import NxWelcome from './nx-welcome';
 
+import { TCheck } from '@monorepo/models';
+
 export function App() {
+  const chec: TCheck = 'asf';
   return (
     <>
       <NxWelcome title="web-app" />
